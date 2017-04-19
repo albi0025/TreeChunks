306c41b0bdfb81c81391858035f1b8fda12be2b1
Andy <andyhburns.com>
2017-04-19 16:02:58 -0600

    we can write a story

--- src/components/Chunk.js
+++ src/components/Chunk.js
@@ -1,19 +1,35 @@
 import React from 'react';
-import { Panel} from 'react-bootstrap';
+import { Panel, Form, Button } from 'react-bootstrap';
+import Chunks from './Chunks';
 
 class Chunk extends React.Component {
 
   constructor() {
     super();
     this.state = {
-      story: []
+      story: [],
+      content: "",
+      chunks: []
     };
 
     this.perpareChunks = this.prepareChunks.bind(this);
+    this.submitChunkHandler = this.submitChunkHandler.bind(this);
+    this.handleContentChange = this.handleContentChange.bind(this);
+    this.getChunks = this.getChunks.bind(this);
   }
 
   componentWillMount(){
     this.prepareChunks(this.props.params.chunkId, []);
+    this.getChunks(this.props.params.chunkId);
+  }
+
+  componentWillReceiveProps(nextProps){
+    this.prepareChunks(nextProps.params.chunkId, []);
+    this.getChunks(nextProps.params.chunkId);
+  }
+
+  handleContentChange(e) {
+    this.setState({content: e.target.value});
   }
 
   prepareChunks(chunkId, story){
@@ -38,14 +54,49 @@ class Chunk extends React.Component {
     });
   }
 
-  //get the chunk then see if it has a parent . loop throuhg that until there is no parent each time return the content
+  getChunks(chunkId){
+    fetch("/getChunks/" + chunkId,{
+      method:"GET",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      }
+    })
+    .then(result => result.json())
+    .then(data => this.setState({chunks: data}));
+  }
+
+  submitChunkHandler(e){
+    e.preventDefault();
+    fetch("/newChunk",{
+      method:"POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        content: this.state.content,
+        parentchunk: this.props.params.chunkId
+      })
+    });
+  }
 
   render() {
 
     return (
-      <Panel>
-        {this.state.story}
-      </Panel>
+      <div>
+        <Panel>
+          {this.state.story}
+        </Panel>
+        <Panel>
+          <Chunks chunks = {this.state.chunks}/>
+        </Panel>
+        <Form>
+          <textarea onChange={this.handleContentChange} type="text" name="content" rows="10" cols="30"  placeholder="Content"/>
+          <br/>
+          <Button onClick={this.submitChunkHandler} type="submit">Submit</Button>
+        </Form>
+      </div>
     );
   }
 }
