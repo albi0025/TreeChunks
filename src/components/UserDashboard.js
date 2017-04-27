374ad542f15e75f89427e253386dbdd0c95989b0
Andy <andyhburns.com>
2017-04-27 15:32:44 -0600

    formating dash

--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Tree from './Tree';
+import DashboardTree from './DashboardTree';
+import { Col } from 'react-bootstrap';
 import { observer, inject } from 'mobx-react';
 
 
@@ -25,7 +26,7 @@ class UserDashboard extends React.Component {
   prepareFollowedTrees(){
     return this.state.followedtrees.map(function(tree){
       return(
-        <Tree key={tree._id} tree={tree}/>
+        <DashboardTree key={tree._id} tree={tree}/>
       );
     },this);
   }
@@ -33,7 +34,7 @@ class UserDashboard extends React.Component {
   prepareCreatedTrees(){
     return this.state.createdtrees.map(function(tree){
       return(
-        <Tree key={tree._id} tree={tree}/>
+        <DashboardTree key={tree._id} tree={tree}/>
       );
     },this);
   }
@@ -82,12 +83,20 @@ class UserDashboard extends React.Component {
 
     return (
       <div className="dashboard-content container-fluid">
-        <div>
-        {followedTrees}
-        </div>
-        <div>
+        <Col xs={12} md={5}>
+
+        <h3 style={{textAlign: "center"}}>Trees you started</h3>
+        <div className="flexy">
         {createdTrees}
         </div>
+        </Col>
+        <Col xs={0} md={2}/>
+        <Col xs={12} md={5}>
+        <h3 style={{textAlign: "center"}}>Your followed trees</h3>
+        <div className="flexy">
+        {followedTrees}
+        </div>
+        </Col>
       </div>
     );
   }
